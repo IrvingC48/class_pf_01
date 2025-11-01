130a8e34a58c6eedb87d1333a83c230b0e826cd7
IrvingC48 <c.ici.3015@hotmail.com>
2025-11-01 10:32:19 -0600

    Se crea archivo de calculadora de propinas, se solicita variables

---
+++ calculadora_propinas.py
@@ -0,0 +1,11 @@
+total_cuenta = input("Ingrese el total de la cuenta en pesos:")
+if not(total_cuenta.isdigit()) or not(total_cuenta > 0):
+    print("Error: Debe ingresar un número positivo para el total de la cuenta.")
+    exit()
+
+def solicitar_propina():
+    porcentaje_propina = input("Ingrese el porcentaje de propina que desea dejar (por ejemplo, 15 para 15%):")
+    if (porcentaje_propina<0) or (porcentaje_propina>100):
+        porcentaje_propina = solicitar_propina()
+    return porcentaje_propina
+
