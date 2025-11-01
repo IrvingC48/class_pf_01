e7263dec4083ad90ca931affa2590ce2523540e3
IrvingC48 <c.ici.3015@hotmail.com>
2025-11-01 10:39:57 -0600

    Se muestran los resultados de propina y total a pagar

--- calculadora_propinas.py
+++ calculadora_propinas.py
@@ -1,3 +1,7 @@
+#Se solicitan datos al usuario, y se validan los siguientes puntos:
+#El total de la cuenta debe ser un número positivo.
+#El porcentaje de propina debe estar entre 0 y 100.
+
 total_cuenta = input("Ingrese el total de la cuenta en pesos:")
 if not(total_cuenta.isdigit()) or not(total_cuenta > 0):
     print("Error: Debe ingresar un número positivo para el total de la cuenta.")
@@ -9,7 +13,15 @@ def solicitar_propina():
         porcentaje_propina = solicitar_propina()
     return porcentaje_propina
 
+
+#Se calcula el porcentaje de propina y el total a pagar
 porcentaje_propina = solicitar_propina()
 
 propina = (total_cuenta * porcentaje_propina) / 100
 total_a_pagar = total_cuenta + propina
+
+# Salida de datos: Se muestra la propina y el total a pagar
+print(f'El monto de la propina es : ${propina} pesos')
+print(f'Total a pagar (incluyendo propina): ${total_a_pagar} pesos')
+print("Gracias por su generosidad!")
+exit()
