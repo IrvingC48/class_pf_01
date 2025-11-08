568d074439d8513ba9a27e923351fe4fab095a95
IrvingC48 <c.ici.3015@hotmail.com>
2025-11-08 10:22:49 -0600

    Casos de condicional if

---
+++ condicionales.py
@@ -0,0 +1,10 @@
+# if - Condicional simple
+# Caso: Verificar si un número es positivo
+numero = int(input("Ingrese un número:"))
+if numero > 0:
+    print("El número es positivo")
+
+# Caso: Validar acceso por edad
+edad = int(input("Ingrese su edad:"))
+if edad >= 18:
+    print("Acceso permitido.")
