6e05b6977a57f419faca0f88268e71b754504d8f
IrvingC48 <c.ici.3015@hotmail.com>
2025-11-08 10:30:12 -0600

    Condicionales dobles if-else

--- condicionales.py
+++ condicionales.py
@@ -8,3 +8,18 @@ if numero > 0:
 edad = int(input("Ingrese su edad:"))
 if edad >= 18:
     print("Acceso permitido.")
+
+# if - else - Condicional compuesto - doble
+# Caso: Comprobar si un número es par o impar
+numero = 5
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar.")
+
+# Caso: Verificar si un usuario está activo
+usuario_activo = False
+if usuario_activo:
+    print("Bienvenido de nuevo.")
+else:
+    print("Por favor, inicia sesión.")
