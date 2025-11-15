37812a5f2f7a082b76e5013a5419aa45678a121d
IrvingC48 <c.ici.3015@hotmail.com>
2025-11-15 10:54:18 -0600

    Estructura for simple

--- estructuras_for_while.py
+++ estructuras_for_while.py
@@ -14,3 +14,23 @@ while True:#validador == False:
  #       validador = True
     else:
         print("Fuera de rango. Intenta de nuevo.")
+
+#Caso de uso  for: Recorrer una lista de productos, detectar los de bajo stock 
+# y calcular el valor total del inventario.
+productos = [
+    {"nombre": "Lapicero", "precio": 5.0, "stock": 12}, #0
+    {"nombre": "Cuaderno", "precio": 25.0, "stock": 3}, #1
+    {"nombre": "Mochila", "precio": 450.0, "stock": 1}, #2
+    {"nombre": "Goma", "precio": 3.5, "stock": 20},     #3
+]
+
+total_inventario = 0
+bajo_stock = []
+
+for producto in productos:
+    if producto["stock"] <=2:
+        bajo_stock.append(producto["nombre"])
+    total_inventario += producto["precio"] * producto["stock"]
+
+print(f'Productos con bajo stock: "{bajo_stock}"')
+print(f'Valor total del inventario: ${total_inventario}')
