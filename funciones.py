c54245bd0174678b6c30d5d5c0fd8cf2c1c75894
IrvingC48 <c.ici.3015@hotmail.com>
2025-12-06 10:48:09 -0600

    Uso de help() en funciones

--- funciones.py
+++ funciones.py
@@ -12,3 +12,25 @@ print(f'El costo del envío es: {precio_final}')
 #Intento de Error
 # print(costo)
 
+#2. Generador de reportes
+
+def generar_titulo(texto, simbolo):
+    """
+    Imprime un texto centrado y rodeado por un símbolo decorativo.
+    Args:
+        texto (str): El título o mensaje principal.
+        simbolo (str): El carácter que se usará para decorar.
+    """
+
+    #Multiplicar un string que repite el carácter.
+    decoracion = simbolo * 10
+    print(f'{decoracion} {texto} {decoracion}')
+
+#Llamada usando help() para ver nuestra documentación (Docstring)
+help(generar_titulo)
+
+#Llamada normal pasando argumentos
+generar_titulo("ANÁLISIS FINAL", "*")
+generar_titulo("ADVERTENCIA", "!")
+
+
