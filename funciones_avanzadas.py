3767043851ca5ac75fd86abe45d2757f0920e212
IrvingC48 <c.ici.3015@hotmail.com>
2025-12-13 12:39:39 -0600

    Uso de valores de retorno

--- funciones_avanzadas.py
+++ funciones_avanzadas.py
@@ -4,3 +4,20 @@ def calcular_total(base, *args):
     return base + total_extras
 
 print(f"Total a pagar: ${calcular_total(100,10,5,20)}")
+
+
+#Uso de valores de retorno
+def analizar_lista(numeros):
+    if not numeros:
+        return None #Protección contra listas vacías
+
+    mayor = max(numeros)
+    menor = min(numeros)
+    promedio = sum(numeros) / len(numeros)
+
+    #Devolucion en una tupla
+    return mayor, menor, promedio
+
+#Desestructuración de la tupla retornada
+res_mayor, res_menor, res_promedio = analizar_lista([10, 8, 9, 5])
+print(f'Mayor: {res_mayor}, Menor: {res_menor}, Promedio: {res_promedio}')
