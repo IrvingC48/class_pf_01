5156020ab500f0d2b7e5ba9f0decbfbcdd006284
IrvingC48 <c.ici.3015@hotmail.com>
2025-12-13 12:46:08 -0600

    Ejercicio de recursividad

--- funciones_avanzadas.py
+++ funciones_avanzadas.py
@@ -1,4 +1,5 @@
 #Uso de args
+#Sumar un precio base más cualquier cantidad de costos extras
 def calcular_total(base, *args):
     total_extras = sum(args)
     return base + total_extras
@@ -7,6 +8,7 @@ print(f"Total a pagar: ${calcular_total(100,10,5,20)}")
 
 
 #Uso de valores de retorno
+#Devolver máximo, mínimo y promedio en una sola llamada.
 def analizar_lista(numeros):
     if not numeros:
         return None #Protección contra listas vacías
@@ -21,3 +23,17 @@ def analizar_lista(numeros):
 #Desestructuración de la tupla retornada
 res_mayor, res_menor, res_promedio = analizar_lista([10, 8, 9, 5])
 print(f'Mayor: {res_mayor}, Menor: {res_menor}, Promedio: {res_promedio}')
+
+
+#Uso de recursividad
+#Cuenta regresiva que imprime ¡Despegue! al llegar a cero.
+def cuenta_regresiva(n):
+    #Caso base
+    if n <= 0:
+        print("¡Despegue!")
+    #Caso recursivo: Se llama a sí misma con n-1
+    else:
+        print(n)
+        cuenta_regresiva(n - 1)
+
+cuenta_regresiva(10)
