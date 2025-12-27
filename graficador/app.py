e8f993bcc19cc1184bbf97e4955df90febbb2fe0
IrvingC48 <c.ici.3015@hotmail.com>
2025-12-27 10:50:28 -0600

    Se genera lógica de la app

---
+++ graficador/app.py
@@ -0,0 +1,25 @@
+from generador_datos import obtener_df
+from graficador import crear_grafica_barras
+
+def main():
+    print("---Iniciando App---")
+
+    try:
+        #1 Obtener los datos del primer módulo
+        df_diabetes = obtener_df()
+
+        #2 Generar una validación simple
+        if not df_diabetes.empty:
+
+            #3 Pasar el df al módulo de graficación
+            crear_grafica_barras(df_diabetes, 'Age', 'Outcome')
+        else:
+            print("Error: El Dataframe está vacío.")
+
+    except Exception as e:
+        print(f'Ocurrió un error inesperado: {e}')
+
+    print("---Fin de la ejecución---")
+
+if __name__ == "__main__":
+    main()
